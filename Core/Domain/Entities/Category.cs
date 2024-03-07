107f2c675c007a557ba842bed22c7cfb54fe6367
busenurdmb <bus.dmb@gmail.com>
2024-03-07 17:12:03 +0300

    Domain Katmanı oluşturuldu. Entityler Yazıldı

---
+++ Core/Domain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
