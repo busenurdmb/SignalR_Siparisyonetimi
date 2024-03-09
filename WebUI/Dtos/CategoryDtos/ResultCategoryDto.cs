f05791b8817896d7484984afa5f5b34d8c6fddfc
busenurdmb <bus.dmb@gmail.com>
2024-03-10 00:41:29 +0300

    Admin Category Crud İşlemleri ve sayfaları yapıldı.

---
+++ WebUI/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebUI.Dtos.CategoryDtos
+{
+	public class ResultCategoryDto
+	{
+		public int CategoryID { get; set; }
+		public string CategoryName { get; set; }
+		public bool Status { get; set; }
+	}
+}
---
+++ WebUI/Dtos/CategoryDtos/UpdateCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebUI.Dtos.CategoryDtos
+{
+	public class UpdateCategoryDto
+	{
+		public int CategoryID { get; set; }
+		public string CategoryName { get; set; }
+		public bool Status { get; set; }
+	}
+}
