ceaf1b58fa22714144f7317e87c707eb69cc60cf
busenurdmb <bus.dmb@gmail.com>
2024-03-18 05:31:05 +0300

    Proje resimleri

--- WebUI/Controllers/MailController.cs
+++ WebUI/Controllers/MailController.cs
@@ -15,6 +15,11 @@ namespace WebUI.Controllers
         [HttpPost]
         public IActionResult Index(CreateMailDto createMailDto)
         {
+            string quillText = createMailDto.Body; // Quill.js editöründen gelen metni buraya yerleştirin
+
+            // Metindeki <p> etiketlerini kaldır
+            string cleanedText = quillText.Replace("<p>", "").Replace("</p>", "");
+            createMailDto.Body= cleanedText;
             MimeMessage mimeMessage = new MimeMessage();
 
             MailboxAddress mailboxAddressFrom = new MailboxAddress("SignalR Rezervasyon", "projekursapi@gmail.com");
@@ -24,7 +29,7 @@ namespace WebUI.Controllers
             mimeMessage.To.Add(mailboxAddressTo);
 
             var bodyBuilder = new BodyBuilder();
-            bodyBuilder.TextBody = createMailDto.Body; ;
+            bodyBuilder.TextBody = createMailDto.Body ;
             mimeMessage.Body = bodyBuilder.ToMessageBody();
 
             mimeMessage.Subject=createMailDto.Subject;
